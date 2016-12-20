903ed1abce08d8ea655fea925991f612a441b0c5
Eduardo <eduardo.s.rodrigues@ist.utl.pt>
2016-12-20 15:23:27 +0000

    OutChannel.scala added

--- src/main/scala/clifton/nodes/OutChannel.scala
+++ src/main/scala/clifton/nodes/OutChannel.scala
@@ -1,8 +1,26 @@
 package clifton.nodes
 
+import clifton.nodes.exceptions.SpaceNotDefined
+import com.zink.fly.FlyPrime
+
 /**
   * Created by #ScalaTeam on 20/12/2016.
   */
-object OutChannel {
-  //TODO
+abstract class OutChannel(marker: String, entryLifeTime : Long = 60*1000) {
+
+  private val space: FlyPrime = getSpace
+  private val out = new ExoEntry(marker,null)
+
+  abstract def getSpace: FlyPrime
+
+  def putObject(obj : Serializable) = {
+    if(space != null && marker != null && obj!= null){
+      out.payload=obj
+      try{
+        space.write(out,entryLifeTime)
+      } catch {
+        case e : Exception => throw new SpaceNotDefined("Output Channel Broken")
+      }
+    }
+  }
 }
