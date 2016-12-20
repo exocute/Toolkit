cf838ba6ba4291d6885a2de44ee5a6ce55c39ab4
Eduardo <eduardo.s.rodrigues@ist.utl.pt>
2016-12-20 15:04:47 +0000

    SignalOutChannel.scala added

---
+++ src/main/scala/clifton/nodes/SignalOutChannel.scala
@@ -0,0 +1,12 @@
+package clifton.nodes
+
+import com.zink.fly.FlyPrime
+
+/**
+  * Created by #ScalaTeam on 20/12/2016.
+  */
+class SignalOutChannel(marker:String) extends InChannel(marker){
+
+  def getSpace : FlyPrime = SpaceCache.getSignalSpace
+
+}
