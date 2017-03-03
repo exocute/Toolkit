5b3d1b6d2bd18357d5cd1d29b10140cf895a7129
Eduardo <eduardo.s.rodrigues@ist.utl.pt>
2017-03-03 15:19:59 +0000

    Exception Added

---
+++ src/main/scala/clifton/graph/exceptions/MissingActivitiesException.scala
@@ -0,0 +1,8 @@
+package clifton.graph.exceptions
+
+/**
+  * Created by #ScalaTeam on 02/03/2017.
+  */
+class MissingActivitiesException(missingActs: List[String]) extends Exception("Activities Missing: " + missingActs) {
+
+}
